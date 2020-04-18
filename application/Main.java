80fcb58771a16d2914f086e25b55bd44c6b4e0a3
Lord-Bubbles <justinhanyueli@gmail.com>
2020-04-17 22:27:11 -0500

    Update Main.java

--- application/Main.java
+++ application/Main.java
@@ -1,1 +1,52 @@
+package application;
+
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	// store any command-line arguments that were entered.
+	// NOTE: this.getParameters().getRaw() will get these also
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// save args example
+		args = this.getParameters().getRaw();
+
+		// Create a vertical box with Hello labels for each args
+		VBox vbox = new VBox();
+		for (String arg : args) {
+			vbox.getChildren().add(new Label("hello " + arg));
+		}
+
+		// Main layout is Border Pane example (top,left,center,right,bottom)
+		BorderPane root = new BorderPane();
+
+		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+		// Add the stuff and set the primary stage
+		primaryStage.setTitle(APP_TITLE);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
 
