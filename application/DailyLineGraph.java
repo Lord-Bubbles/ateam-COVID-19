408a8b545e2fa2f156197ad5f88ec07ce7cf2d8a
Caidia <kevinyu.us@gmail.com>
2020-04-30 13:10:12 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/Lord-Bubbles/ateam-COVID-19""
    This reverts commit 42e0741dc14b81a013450c6e427fe089b6320495.

--- application/DailyLineGraph.java
+++ application/DailyLineGraph.java
@@ -1,3 +1,4 @@
+
 package application;
 
 import java.util.List;
@@ -11,6 +12,13 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * This class is used to represent a Daily Line Graph of a country that plots
+ * both the confirmed cases and the death rates of COVID19 across four months.
+ * 
+ * @author paul
+ *
+ */
 public class DailyLineGraph extends Application {
 
 	public static final int WINDOW_WIDTH = 400;
@@ -18,41 +26,62 @@ public class DailyLineGraph extends Application {
 	final CategoryAxis xAxis = new CategoryAxis();
 	final NumberAxis yAxis = new NumberAxis();
 	final LineChart chart = new LineChart(xAxis, yAxis);
+	private XYChart.Series series1; // data for confirmed cases
+	private XYChart.Series series2; // data for death rates
 
-	
+	/**
+	 * This constructor is used to set the labels and titles of the graphs
+	 * 
+	 */
 	public DailyLineGraph() {
 		xAxis.setLabel("Week");
 		chart.setTitle("COVID 19 WEEKLY TRENDS");
-		XYChart.Series series1 = new XYChart.Series();
+		series1 = new XYChart.Series();
 		series1.setName("Number of Cases");
-		List<Event> dataCases = CsvReaderWriter.readCsv("us.csv");
-		for (Event e : dataCases) {
-			series1.getData().add(
-					new XYChart.Data(e.getDate().substring(6), e.getCases()));
-		}
-		XYChart.Series series2 = new XYChart.Series();
+		series2 = new XYChart.Series();
 		series2.setName("Number of Deaths");
-		for(Event e : dataCases) {
-			series2.getData().add(new XYChart.Data(e.getDate().substring(6), e.getDeaths()));
-		}
-		chart.getData().addAll(series1, series2);
 	}
-	
+
+	/**
+	 * This method is used if you want to individually start up the daily line
+	 * graph. This method is not explicitly used.
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(chart, 800, 600);;
+		Scene scene = new Scene(chart, 800, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
+	/**
+	 * This main method is used to launch the graph program individually. This
+	 * method is also not explicitly used.
+	 * 
+	 * @param args - String input arguments not used.
+	 */
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
-	public LineChart getDailyGraph() {
+	/**
+	 * This method is used to return a chart of daily line graph. This method
+	 * uses a csv reader to retrieve a list of the specified country's confirmed
+	 * cases and death rates.
+	 * 
+	 * @param country - country to graph
+	 * @return chart - a chart of both data points plotted
+	 */
+	public LineChart getDailyLineGraph(String country) {
+		List<Event> countryStats = CsvReaderWriter
+				.readCsv("confirmedglobal.csv", "deathsglobal.csv", country);
+		for (Event e : countryStats) {	
+			series1.getData().add(		// confirmed cases
+					new XYChart.Data(e.getDate().substring(6), e.getCases()));
+			series2.getData().add(		// death rates
+					new XYChart.Data(e.getDate().substring(6), e.getDeaths()));
+		}
+		chart.getData().addAll(series1, series2);
 		return chart;
 	}
-	
 }
--- application/MonthlyLineGraph.java
+++ application/MonthlyLineGraph.java
@@ -13,6 +13,13 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * This class is used to represent a Monthly Line Graph of a country that plots
+ * both the confirmed cases and the death rates of COVID19 across four months.
+ * 
+ * @author paul
+ *
+ */
 public class MonthlyLineGraph extends Application {
 
 	public static final int WINDOW_WIDTH = 400;
@@ -21,49 +28,73 @@ public class MonthlyLineGraph extends Application {
 	final CategoryAxis xAxis = new CategoryAxis();
 	final NumberAxis yAxis = new NumberAxis();
 	final LineChart chart = new LineChart(xAxis, yAxis);
+	private XYChart.Series series1; // confirmed cases
+	private XYChart.Series series2; // death rates
 
+	/**
+	 * This constructor is used to set the labels and titles of the graphs
+	 */
 	public MonthlyLineGraph() {
 		xAxis.setLabel("Month");
 		chart.setTitle("COVID 19 MONTHLY TRENDS");
-
-		XYChart.Series series1 = new XYChart.Series();
+		series1 = new XYChart.Series();
+		series2 = new XYChart.Series();
 		series1.setName("Number of Cases");
-		XYChart.Series series2 = new XYChart.Series();
 		series2.setName("Number of Deaths");
-		List<Event> weeklyCases = CsvReaderWriter.readCsv("us.csv");
+	}
+
+	/**
+	 * This method is used if you want to individually start up the monthly line
+	 * graph. This method is not explicitly used.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(chart, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	/**
+	 * This main method is used to launch the graph program individually. This
+	 * method is also not explicitly used.
+	 * 
+	 * @param args - String input arguments not used.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * This method is used to return a chart of monthly line graph. This method
+	 * uses a csv reader to retrieve a list of the specified country's confirmed
+	 * cases and death rates.
+	 * 
+	 * @param country - country to graph
+	 * @return chart - a chart of both data points plotted
+	 */
+	public LineChart getMonthlyLineGraph(String country) {
+		List<Event> countryStats = CsvReaderWriter
+				.readCsv("confirmedglobal.csv", "deathsglobal.csv", country);
 		List<Event> monthlyCases = new ArrayList<Event>();
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++) { // adding up all the days to months
 			monthlyCases.add(new Event());
 			monthlyCases.get(i).setDate(MONTHS[i]);
 		}
-		for (int i = 0; i < weeklyCases.size(); i++) {
+		for (int i = 0; i < countryStats.size(); i++) {
 			int j = Integer
-					.parseInt(weeklyCases.get(i).getDate().substring(6, 7)); // get
-																				// month
-			monthlyCases.get(j - 1).setCases(weeklyCases.get(i).getCases());
-			monthlyCases.get(j - 1).setDeaths(weeklyCases.get(i).getDeaths());
+					.parseInt(countryStats.get(i).getDate().substring(6, 7)); // month
+			monthlyCases.get(j - 1).setCases(countryStats.get(i).getCases());
+			monthlyCases.get(j - 1).setDeaths(countryStats.get(i).getDeaths());
 		}
 		for (Event e : monthlyCases) {
 			series1.getData().add(new XYChart.Data(e.getDate(), e.getCases()));
 			series2.getData().add(new XYChart.Data(e.getDate(), e.getDeaths()));
 		}
 		chart.getData().addAll(series1, series2);
-	}
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		BorderPane root = new BorderPane();
-        Scene scene = new Scene(chart, 800, 600);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	public LineChart getMonthlyGraph() {
 		return chart;
+
 	}
 
 }
